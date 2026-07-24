f89c486858d553650750498777e19db6882434d9
Piotr <piotr.brejnak@gmail.com>
2026-07-24 14:18:28 +0200

    feat(mat): 8 działów JSON + pełny rejestr.js (it.2 T1)

--- repetytorium - matematyka/app/src/content/matematyka/rejestr.js
+++ repetytorium - matematyka/app/src/content/matematyka/rejestr.js
@@ -1,8 +1,23 @@
 import liczby from "./dzialy/liczby.json";
+import ulamki from "./dzialy/ulamki.json";
+import potegi from "./dzialy/potegi.json";
+import procenty from "./dzialy/procenty.json";
+import algebra from "./dzialy/algebra.json";
+import rownania from "./dzialy/rownania.json";
+import geometriaPlaska from "./dzialy/geometria-plaska.json";
+import pitagoras from "./dzialy/pitagoras.json";
+import geometriaPrzestrzenna from "./dzialy/geometria-przestrzenna.json";
 
 export const DZIALY = {
   liczby,
-  // TODO it.2: ulamki, potegi, procenty, algebra, rownania, geometria-plaska, pitagoras, geometria-przestrzenna
+  ulamki,
+  potegi,
+  procenty,
+  algebra,
+  rownania,
+  "geometria-plaska": geometriaPlaska,
+  pitagoras,
+  "geometria-przestrzenna": geometriaPrzestrzenna,
 };
 
 export function material(id) {
